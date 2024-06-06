aa5b721fa2e22f852725ebf4ba88981fcb6a62f4
joel kalema <joelkalema63@gmail.com>
2024-06-07 00:28:54 +0200

    setup the home page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,1 +1,105 @@
+import { View, Image, Text } from 'react-native';
+import React from 'react';
+import { Tabs } from 'expo-router';
+import { icons } from '@/constants';
+import tw from 'twrnc';
+
+const TabIcon = ({ icon, color, name, focused }) => {
+    return (
+        <View style={tw`items-center justify-center gap-2`}>
+            <Image
+                source={icon}
+                resizeMode='contain'
+                tintColor={color}
+                style={tw`w-6 h-6`}
+            />
+            <Text style={[tw`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`, { color }]}>
+                {name}
+            </Text>
+        </View>
+    );
+};
+
+const Tabslayout = () => {
+    return (
+        <Tabs
+            screenOptions={{ tabBarShowLabel: false,
+            tabBarActiveTintColor: '#FFA001',
+            tabBarInactiveTintColor: '#CDCDE0',
+            tabBarStyle: {
+                backgroundColor: '#161622',
+                borderTopWidth: 1,
+                borderTopColor: '#232533',
+                height: 84, 
+            }
+             }}
+        >
+            <Tabs.Screen
+                name='home'
+                options={{
+                    title: 'Home',
+                    headerShown: false,
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabIcon
+                            icon={icons.home}
+                            color={color}
+                            name="Home"
+                            focused={focused}
+                        />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name='bookmark'
+                options={{
+                    title: 'Bookmark',
+                    headerShown: false,
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabIcon
+                            icon={icons.bookmark}
+                            color={color}
+                            name="Bookmark"
+                            focused={focused}
+                        />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name='create'
+                options={{
+                    title: 'Create',
+                    headerShown: false,
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabIcon
+                            icon={icons.plus}
+                            color={color}
+                            name="Create"
+                            focused={focused}
+                        />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name='profile'
+                options={{
+                    title: 'Profile',
+                    headerShown: false,
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabIcon
+                            icon={icons.profile}
+                            color={color}
+                            name="Profile"
+                            focused={focused}
+                        />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
+};
+
+export default Tabslayout;
 
--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,46 @@ import { Image, StyleSheet, Platform, View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Link } from 'expo-router';
 import tw from 'twrnc';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native-gesture-handler';
+import { images } from '@/constants';
 
-export default function HomeScreen() {
+export default function App() {
   return (
-    <View style={tw`flex-1 justify-center items-center`}>
-      <Text style={tw`font-extrabold text-3xl font-pblack mb-6`}>Dev</Text>
-        <StatusBar style='auto' />
-        <Link href="/profile">Go to profile</Link>
-    </View>
+    <SafeAreaView style={tw`bg-[#161622] h-full`}>
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View style={tw`w-full justify-center items-center h-full px-4`}>
+          <Image
+            source={images.logo}
+            style={tw`w-[130px] h-[84px]`}
+            resizeMode='contain'
+          />
+
+          <Image
+            source={images.cards}
+            style={tw`max-w-[380px] w-full h-[300px]`}
+            resizeMode='contain'
+          />
+
+          <View style={tw`relative mt-5`}>
+            <Text style={tw`text-3xl text-white font-bold text-center`}>Discover Endless Possibilities with{' '}
+              <Text style={tw`text-[#FFA001]`}>Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              style={tw`w-[136px] w-full h-[15px] absolute -bottom-3 -right-30`}
+              resizeMode='contain'
+            />
+          </View>
+
+          <Text style={tw`text-sm font-pregular text-gray-100 mt-7 text-center`}>
+            Where creativity meets innovation:
+            embark on a jorney of limitatless exploration with Aora
+          </Text>
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
